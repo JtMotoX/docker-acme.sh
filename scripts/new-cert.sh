90a1add0f4531b3a54fee88471628bc33b750080
JtMotoX <7191259+JtMotoX@users.noreply.github.com>
2022-05-17 20:29:07 -0700

    use cron instead of ofelia

--- scripts/new-cert.sh
+++ scripts/new-cert.sh
@@ -4,35 +4,81 @@
 cd "$(dirname "$0")"
 cd ..
 
+# SET SOME DEFAULTS
+DEFAULT_DNS_PROVIDER="dns_dynu"
+
+# DISPLAY USAGE
+usage() {
+	cat << EOF 1>&2
+---
+Usage: $(basename $0)
+	-d, --domain	the domain to generate the cert for
+	-p, --provider	the dns provider (default: ${DEFAULT_DNS_PROVIDER})
+	-h, --help	display this help
+EOF
+	exit 1;
+}
+
+# SHOW USAGE IF NO ARGUMENTS
+if [ $# -eq 0 ]; then usage; fi
+
+# GET ARGUMENTS
+options=$(getopt -o hd:p: --longoptions help,domain:,provider: -- "$@")
+[ $? -eq 0 ] || { 
+    echo "Incorrect options provided"
+    exit 1
+}
+eval set -- "$options"
+while true; do
+	case "$1" in
+		-d | --domain)
+			DOMAIN="$2"
+			shift
+			;;
+		-p | --provider)
+			DNS_PROVIDER="$2"
+			shift
+			;;
+		--help)
+			usage
+			;;
+		--)
+			shift
+			break
+			;;
+	esac
+	shift
+done
+
 # MAKE SURE DOMAIN WAS GIVEN
-DOMAIN="$1"
 if [ "${DOMAIN}" = "" ]; then
-	echo "Please pass the domain to generate a cert as the first parameter."
-	exit 1
+	echo "ERROR: domain not provided"
+	usage
 fi
 
-# CREATE DYNU.ENV FILE IF NEEDED
-unset Dynu_ClientId
-unset Dynu_Secret
-test -f "./dynu.env" && . ./dynu.env
-if { echo "${Dynu_ClientId}" | grep -E "^(|\*\*\*)$" >/dev/null 2>&1; } || { echo "${Dynu_Secret}" | grep -E "^(|\*\*\*)$" >/dev/null 2>&1; }; then
-	printf "Dynu_ClientId=\"***\"\nDynu_Secret=\"***\"\n" >dynu.env
-	echo "Please update the dynu.env file"
-	exit 1
+# SET DEFAULT DNS PROVIDER IF NON GIVEN
+if [ "${DNS_PROVIDER}" = "" ]; then
+	DNS_PROVIDER="${DEFAULT_DNS_PROVIDER}"
 fi
-echo "NOTE: Make sure to delete the 'dynu.env' file after successfully generating a cert."
+
+# CHECK DYNU VARIABLES IF USING DYNU DNS
+if [ "${DNS_PROVIDER}" = "dns_dynu" ]; then
+	Dynu_ClientId=$(docker-compose run --rm acme-sh sh -c 'echo "$Dynu_ClientId" | tr -d "\n"')
+	if { echo "${Dynu_ClientId}" | grep -E "^(|\*\*\*)$" >/dev/null 2>&1; }; then
+		echo "ERROR: You need to provide the 'Dynu_ClientId' in the '.env' file."
+		exit 1
+	fi
+	Dynu_Secret=$(docker-compose run --rm acme-sh sh -c 'echo "$Dynu_Secret" | tr -d "\n"')
+	if { echo "${Dynu_Secret}" | grep -E "^(|\*\*\*)$" >/dev/null 2>&1; }; then
+		echo "ERROR: You need to provide the 'Dynu_Secret' in the '.env' file."
+		exit 1
+	fi
+fi
 
 # GET A CERT
-docker run --rm \
-	-v "$(pwd)/acme-logs:/var/log/acme-logs" \
-	-v "$(pwd)/acme.sh:/acme.sh" \
-	--env-file "$(pwd)/dynu.env" \
-	neilpang/acme.sh:latest \
+docker-compose run --rm acme-sh \
+	acme.sh \
 		--issue \
 		-d ${DOMAIN} \
 		-d *.${DOMAIN} \
-		--home "/acme.sh" \
-		--config-home "/acme.sh/configs" \
-		--cert-home "/acme.sh/certs" \
-		--server letsencrypt \
-		--dns dns_dynu
+		--dns ${DNS_PROVIDER}
